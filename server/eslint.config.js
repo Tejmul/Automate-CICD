a2b656aa810d1ec0e3793ccfa99bc48f812caadc
Tejmul Movin <tejmulmovin@gmail.com>
2026-03-18 23:52:06 +0530

    eslint configs

---
+++ server/eslint.config.js
@@ -0,0 +1,42 @@
+const js = require("@eslint/js");
+
+module.exports = [
+  {
+    ignores: ["node_modules/**", "coverage/**"],
+  },
+  js.configs.recommended,
+  {
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: "commonjs",
+    },
+    plugins: {
+      jest: require("eslint-plugin-jest"),
+    },
+    rules: {
+      "no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
+    },
+  },
+  {
+    files: ["tests/**"],
+    languageOptions: {
+      globals: {
+        describe: "readonly",
+        it: "readonly",
+        test: "readonly",
+        expect: "readonly",
+        beforeAll: "readonly",
+        afterAll: "readonly",
+        beforeEach: "readonly",
+        afterEach: "readonly",
+      },
+    },
+    rules: {
+      "jest/no-disabled-tests": "warn",
+      "jest/no-focused-tests": "error",
+      "jest/no-identical-title": "error",
+      "jest/valid-expect": "error",
+    },
+  },
+];
+
